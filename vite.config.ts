08395a34bfdb30ebb1eb9ce012649aca3cebb647
Sebastian mardones <se.mardones@duouc.cl>
2026-01-04 17:39:48 -0300

    fix: update base and publish new docs

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Pasteleria_Frontend/",
+  base: "/origenes-restobar/",
   build: {
     outDir: "docs",
     emptyOutDir: true,
