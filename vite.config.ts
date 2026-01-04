73fdd23f0a9a33ae9f673d34a2420f460c5c8036
Sebastian mardones <se.mardones@duouc.cl>
2026-01-04 13:10:16 -0300

    deploy: build to docs for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Pasteleria_Frontend/", 
+  base: "/Pasteleria_Frontend/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
